c4ebbc5afe42bec960b50ffd723d9a001c51e03a
fodiloglu-a <mutalip.fodiloglu@gmail.com>
2023-04-24 01:53:57 +0300

    Initial commit

---
+++ src/main/java/com/fdlSpring/FdlSpringBlog.java
@@ -0,0 +1,13 @@
+package com.fdlSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FdlSpringBlog {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FdlSpringBlog.class, args);
+	}
+
+}
---
+++ src/test/java/com/fdlSpring/FdlSpringBlogTests.java
@@ -0,0 +1,13 @@
+package com.fdlSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FdlSpringBlogTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
